f000fa2e351cc01d9c780aec6ac19f7aeb0fcc16
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-01-07 16:42:19 +0400

    started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <div className="App">
-
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <header>
+            <div className='header-contacts'>
+                <div>Need help? Call us: (+98) 0234 456 789</div>
+                <div><div>Our store</div><div>Our store</div></div>
+
+            </div>
+            <div></div>
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
