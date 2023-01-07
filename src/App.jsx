883454048e5662a1a1a1d80646d5bde92b66cc97
Tahir Y. Dibirov <tahiryd@code.local>
2023-01-07 04:44:08 -0800

    Folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Footer from './components/Footer/Footer'
+// import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-
+      <Header/>
+        {/* <Routes>
+          <Route path='/'  />
+        </Routes> */}
+        <Footer />
     </div>
   )
 }
