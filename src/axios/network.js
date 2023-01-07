49d23a812475fb88933c8800bd60efb087479624
Haqverdi56 <qhaqverdi56@gmail.com>
2023-01-07 17:27:26 +0400

    New

--- src/axios/network.js
+++ src/axios/network.js
@@ -2,7 +2,7 @@ export const instance = axios.create({
     baseURL: 'https://some-domain.com/api/',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
-  });
+});
   
 
 
--- src/components/Banner/BProduct.jsx
+++ src/components/Banner/BProduct.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import canon from '../../assets/img/canon.svg'
 
 function BProduct() {
   return (
     <div>
-      P
+      <div>
+        <h1>Canon Camera</h1>
+        <button>Shop Now</button>
+        <button>View more</button>
+      </div>
+      <div>
+        <img src={canon} alt="" />
+      </div>
     </div>
   )
 }
