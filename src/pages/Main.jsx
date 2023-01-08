6bb074972f326f22c02a2cc269ea88bbd6a4338d
Tahir <dibirovtahir@gmail.com>
2023-01-08 19:52:23 +0400

    #

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BProduct from '../components/Banner/BProduct'
 import { Products } from '../components/Products/Products'
 import ProductsFilter from '../components/ProductsFilter/ProductsFilter'
+
 import './Main.css'
 
 export const Main = () => {
