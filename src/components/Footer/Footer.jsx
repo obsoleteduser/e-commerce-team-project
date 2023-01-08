7b8b2a68180191805f4adc106416166ee34f18f9
Mir-Ismayil-Asadullayev <mirim@code.edu.az>
2023-01-09 00:35:51 +0400

    header section responsive design ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import "./Footer.css"
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <div className='footer-subscribe-section'>
+        <div>Subscribe Newsteller</div>
+        <div><button>Email address</button><img className='footer-subscribe-section-telegram' src='src\assets\img\telegram.svg'></img></div>
+        <div><img className='footer-subscribe-section-headphone' src='src\assets\img\headphones-simple-solid.svg'></img>Call us 24/7:<br></br> (+62) 0123 567 789</div>
+      </div>
+      <div></div>
+    </footer>
   )
 }
 
