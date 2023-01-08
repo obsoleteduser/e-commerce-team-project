ba060740de5d738eac6d0bb46388169eb4fb9a96
Tahir <dibirovtahir@gmail.com>
2023-01-08 03:02:56 -0500

    product filter UI is done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import './Products.css'
+import './Product.css'
 
 const Products = () => {
   return (
-    <div  className='Products'>
+    <div  className='products-container'>
       <div className="product-controller">
-        <p>Popular products</p>
-        
+        <p className='section-title'>Popular products</p>
+      <div className="buttons">
+      <button className='cameras cbtn'>Cameras</button>
+        <button className='laptops cbtn'>Laptops</button>
+        <button className='tablets cbtn'>Tablets</button>
+        <button className='mouse cbtn'>Mouse</button>
+      </div>
       </div>
     </div>
   )
