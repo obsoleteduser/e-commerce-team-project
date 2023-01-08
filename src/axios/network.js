d46645068f9eaaaf75acd2c8fc90f8bb3070c45a
Tahir <dibirovtahir@gmail.com>
2023-01-08 07:52:25 -0500

    Responsivness satisfied

--- src/axios/network.js
+++ src/axios/network.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL: 'https://fakestoreapi.com/',
-    timeout: 1000,
+    // timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
   });
   
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { network } from '../../axios/network'
+import { GlobalContext } from '../../GlobalStateManagement'
 import './Product.css'
 
 export const Products = () => {
+
+    const {products, setProducts} = useContext(GlobalContext)
+    const fetchData = async ()=>{
+        const data = await network.getProducts('./products/category/electronics?limit=8')
+        console.log(data.data)
+        setProducts(data.data)
+    }
+
+    useEffect(()=>{
+        fetchData()
+    }, [])
+
+  
+
   return (
       <div className="products">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+        {
+            products.map(product =>(
+                <div key={product.id} className='card'>
+                    <img src={product.image}/>
+                    <p className='product-title'>{product.title}</p>
+                    <p>{product.price}$</p>
+                </div>
+            ) )
+        }
       </div>
   )
 }
