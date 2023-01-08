cc4cb2469fd17cfde161bc1573d2fb30172adec2
Tahir <dibirovtahir@gmail.com>
2023-01-08 19:01:15 +0400

    merged layers

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import { Main } from './pages/Main'
-// import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
