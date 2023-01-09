2043604cd5a232366ffc0c44c3eadcf2b3d66d60
Tahir <dibirovtahir@gmail.com>
2023-01-09 10:46:02 +0400

    Second page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Products } from '../components/Products/Products'
+import { GlobalContext } from '../GlobalStateManagement'
+import { Card } from '../components/Card/Card'
+import { ClipLoader } from 'react-spinners'
 
 export const Catalog = () => {
+
+  const {products, loading} = useContext(GlobalContext)
+
   return (
-    <div>Catalog</div>
+    <div className="catalog">
+      <Products>
+      { !loading ?
+            products.map(product =>(
+               <Card key={product.id} product={product}></Card> 
+            ) )
+            : <div className="loader">
+                <ClipLoader
+            color="#1B5A7D"
+            size={100}
+          />
+            </div>
+        }
+      </Products>
+    </div>
   )
 }
