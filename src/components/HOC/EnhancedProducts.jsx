8c4a1d743ba5baac37401d92f0993602b8a250cf
Tahir <dibirovtahir@gmail.com>
2023-01-09 11:23:33 +0400

    predeploy preparings

--- src/components/HOC/EnhancedProducts.jsx
+++ src/components/HOC/EnhancedProducts.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const withNewProp = (Component) => {
   return function NewComponent(props){
-    return <Component {...props} grid="repeat(2, 1fr)"/>
+    return <Component {...props} grid="repeat(3, 1fr)"/>
   }
 }
